a6f1eb6edf5cd888a81d5070461dc0d4a51fd807
giorgi beruashvili <giorgiberuashvili89@yahoo.com>
2023-05-31 11:19:46 +0400

    commit 2

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ let inputBill = document.querySelector('.input-bill');
 const buttons = document.querySelectorAll('.button-percent');
 let custom = document.querySelector('.custom-input')
 let inputNumberOfPeople = document.querySelector('.input-number-people');
-let tipAmount = document.querySelector('.tip-amount-numbers');
-let total = document.querySelector('.total-numbers');
+let tipAmount = document.querySelector('.tip-amount-result');
+let total = document.querySelector('.total-result');
 let reset = document.querySelector('.reset-all');
 let rename;
 
-inputBill.addEventListener('input', () => {
+inputBill.addEventListener('input', (event) => {
     calculateTipAmount()
+    const inputValue = event.target.value;
+    const sanitizedValue = inputValue.replace(/-/g, ''); 
+    event.target.value = sanitizedValue;
+
 })
 
 buttons.forEach((element) => {
@@ -25,8 +29,11 @@ custom.addEventListener('input', () => {
     calculateTipAmount()
 })
 
-inputNumberOfPeople.addEventListener('input', () => {
+inputNumberOfPeople.addEventListener('input', (event) => {
     calculateTipAmount()
+    const inputValue = event.target.value;
+    const sanitizedValue = inputValue.replace(/-/g, ''); 
+    event.target.value = sanitizedValue;
 })
 
 const calculateTipAmount = () => {
@@ -35,6 +42,17 @@ const calculateTipAmount = () => {
         const totalResult = ((inputBill.value / inputNumberOfPeople.value) + tipAmountResult)
         tipAmount.innerHTML = '$' + tipAmountResult.toFixed()
         total.innerHTML = '$' + totalResult.toFixed()
+    }
+    if(inputNumberOfPeople.value == 0) {
+        inputNumberOfPeople.style.borderColor = "red";
+    }else {
+        inputNumberOfPeople.style.borderColor = "#26c2ae";
+    }
+    console.log(inputNumberOfPeople.value)
+    if(inputBill.value == 0) {
+        inputBill.style.borderColor = "red"
+    }else {
+        inputBill.style.borderColor = "#26c2ae"
     }
 }
 

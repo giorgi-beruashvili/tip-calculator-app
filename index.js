71e96102cc7efcb16c823f01296a19aab987195b
giorgi beruashvili <giorgiberuashvili89@yahoo.com>
2023-05-31 13:05:10 +0400

    comit 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let inputBill = document.querySelector('.input-bill');
 const buttons = document.querySelectorAll('.button-percent');
-let custom = document.querySelector('.custom-input')
+let custom = document.querySelector('.custom-input');
+let eror = document.querySelector('.eror-div p');
 let inputNumberOfPeople = document.querySelector('.input-number-people');
 let tipAmount = document.querySelector('.tip-amount-result');
 let total = document.querySelector('.total-result');
@@ -34,29 +35,36 @@ inputNumberOfPeople.addEventListener('input', (event) => {
     const inputValue = event.target.value;
     const sanitizedValue = inputValue.replace(/-/g, ''); 
     event.target.value = sanitizedValue;
+    console.log(sanitizedValue)
 })
 
 const calculateTipAmount = () => {
+    if(inputNumberOfPeople.value === '0') {
+        inputNumberOfPeople.style.borderColor = "red";
+        eror.style.display = 'block';
+        return;
+    }else {
+        inputNumberOfPeople.style.borderColor = "#26c2ae";
+        eror.style.display = 'none';
+    }
+    if(inputBill.value === '0') {
+        inputBill.style.borderColor = "red"
+        eror.style.display = 'block';
+        return;
+    }else {
+        inputBill.style.borderColor = "#26c2ae"
+        eror.style.display = 'none';
+    }
     if(inputBill.value && rename && inputNumberOfPeople.value) {
         const tipAmountResult = ((inputBill.value * (rename / 100)) /inputNumberOfPeople.value)
         const totalResult = ((inputBill.value / inputNumberOfPeople.value) + tipAmountResult)
         tipAmount.innerHTML = '$' + tipAmountResult.toFixed()
         total.innerHTML = '$' + totalResult.toFixed()
     }
-    if(inputNumberOfPeople.value == 0) {
-        inputNumberOfPeople.style.borderColor = "red";
-    }else {
-        inputNumberOfPeople.style.borderColor = "#26c2ae";
-    }
-    console.log(inputNumberOfPeople.value)
-    if(inputBill.value == 0) {
-        inputBill.style.borderColor = "red"
-    }else {
-        inputBill.style.borderColor = "#26c2ae"
-    }
 }
 
 reset.addEventListener('click', () => {
+    eror.innerHTML = ""
     inputBill.value = ""
     inputNumberOfPeople.value = ""
     tipAmount.innerHTML = "$0.00"
